3505f32b856341c05c63f9801ff23889ffa9d251
Brian Hall <brianhall930@gmail.com>
2024-07-11 21:39:43 -0500

    Added Screen 2 Home View

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Brian Hall on 7/11/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
