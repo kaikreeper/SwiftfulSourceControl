60658c54250ba0e216c05468bbcb5a73d44408f6
Brian Hall <brianhall930@gmail.com>
2024-07-11 22:45:39 -0500

    [Feature] Changed heart to black

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         VStack {
             Image(systemName: "heart.fill")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
+                .foregroundColor(.black)
             RoundedRectangle(cornerRadius: 50)
         }
         .padding()
