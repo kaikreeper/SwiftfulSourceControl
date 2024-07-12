a855938b9ab847bdb30db6873e1c1d6fd2af0fc2
Brian Hall <brianhall930@gmail.com>
2024-07-12 15:18:18 -0500

    [Clean] Update documentation

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -13,7 +13,9 @@
  Stash = Save changes for later
  Push = Send local commits to remote repo
  Pull = Get Remote commits to local repo
- 
+ Merge = Joining two different branches
+ Rebase = Moving one branch on top of another branch
+ Cherry picking = Duplicating one commit from one branch to another
  
  COMMIT MESSAGES
  [Feature] Description of the feature
