0e3230cadf3e3a2ddbed0e5ecc6cb229d607053d
Brian Hall <brianhall930@gmail.com>
2024-07-12 15:49:39 -0500

    [Feature] Added settings view

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -35,7 +35,7 @@ import SwiftUI
 
 struct HomeView: View {
     
-    @State private var title: String = " Hello"
+    @State private var title: String = " Hello bro"
     
     var body: some View {
         Text("HI")
