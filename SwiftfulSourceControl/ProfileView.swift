d458514c1ef8fbfaa8d54cab11da9da26dc1a1ad
Brian Hall <brianhall930@gmail.com>
2024-07-12 20:46:59 -0500

    [Feature] Add isPremium bool

--- SwiftfulSourceControl/ProfileView.swift
+++ SwiftfulSourceControl/ProfileView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var isPremium: Bool = false
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
